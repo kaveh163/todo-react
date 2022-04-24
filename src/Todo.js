f5cfc656efa44842cd521910390584feb773c2cb
Kaveh <kavehfrounchi@gmail.com>
2022-04-24 18:30:58 +0430

    Added local Storage to the Todos

--- src/Todo.js
+++ src/Todo.js
@@ -20,13 +20,25 @@ let count = 0;
 const reducer = (state, action) => {
   switch (action.type) {
     case "removeItem": {
-      return state.filter((item) => {
-        if (item.id === action.id) {
-          return false;
-        } else {
-          return true;
-        }
-      });
+      // return state.filter((item) => {
+      //   if (item.id === action.id) {
+      //     return false;
+      //   } else {
+      //     return true;
+      //   }
+      // });
+      const removeValIndex = action.id -1;
+      console.log('remove', removeValIndex);
+      console.log('stateRemove', state);
+      state.splice(removeValIndex, 1);
+      count = state.length;
+      return state.map((item, index) => {
+        let obj ={};
+        obj['id'] = index + 1;
+        obj['todo'] = item.todo;
+        return obj
+      })
+      
     }
     case "update": {
       return state.map((item) => {
@@ -40,11 +52,16 @@ const reducer = (state, action) => {
     }
     case "addInput": {
       let obj = {
-        id : ++count,
-        todo: action.add
-      }
+        id: ++count,
+        todo: action.add,
+      };
       state = [...state, obj];
       return state;
+    }
+    case "lsSet": {
+      console.log('reducestate', state);
+      console.log('reducestate2', action.data);
+      return action.data;
     }
     default:
       return state;
@@ -58,12 +75,12 @@ function Todo() {
   };
   const handleInput = useRef([]);
   const addInput = useRef();
-  const handleAdd =() => {
-    console.log('addInput',addInput.current.value);
+  const handleAdd = () => {
+    console.log("addInput", addInput.current.value);
     const addValue = addInput.current.value;
-    dispatch({type:'addInput', add: addValue})
-    addInput.current.value = '';
-  }
+    dispatch({ type: "addInput", add: addValue });
+    addInput.current.value = "";
+  };
   const handleUpdate = (update, item) => {
     // const item = e.target.getAttribute("data-id")
     // console.log(handleInput.current);
@@ -71,7 +88,59 @@ function Todo() {
     update.value = "";
     dispatch({ type: "update", id: item.id, updateVal: updateValue });
   };
-  
+  useEffect(() => {
+    // console.log("useEffect");
+    // let data = JSON.parse(localStorage.getItem("data"));
+    // console.log("data 1", data);
+    // if (localStorage.getItem("data") === null) {
+    //   data = [];
+    //   localStorage.setItem("data", JSON.stringify(data));
+    // } else {
+      // if(state.length === 0) {
+      //   dispatch({type: "lsSet", data: data})
+      // } else {
+      //   localStorage.setItem('data', JSON.stringify(state));
+      // }
+      // if (state.length === 0) {
+      //   console.log("state");
+      //   let refData = JSON.parse(localStorage.getItem("data"));
+      //   console.log(refData);
+      //   dispatch({ type: "lsSet", data: refData });
+      // } else {
+      //   data = [...state];
+      //   localStorage.setItem("data", JSON.stringify(data));
+      // }
+    // }
+
+    let array;
+    let bool;
+    // console.log("state1", state);
+    
+    array = JSON.parse(localStorage.getItem("storeName"));
+    // console.log('array', array);
+    
+    if (localStorage.getItem("storeName") === null) {
+      array = [];
+      localStorage.setItem("storeName", JSON.stringify(array));
+    } else {
+      
+      // console.log("state2", state)
+      if (state.length === 0) {
+        // console.log("state3", state);
+        let data = JSON.parse(localStorage.getItem("storeName"));
+        count = data.length;
+        
+        // console.log('data', data);
+        dispatch({ type: "lsSet", data: data });
+      } else {
+        // console.log('state4', state)
+        array = [...state];
+        // console.log('array2', array);
+        localStorage.setItem("storeName", JSON.stringify(array));
+      }
+    }
+  }, [state]);
+
   return (
     <>
       <div>
@@ -127,12 +196,13 @@ function Todo() {
         {toggle && (
           <div className="d-flex justify-content-center">
             <div className="input-group w-50">
-              <input className="form-control" ref={addInput}/>
-              <button className="btn btn-danger" onClick={handleAdd}>Add</button>
+              <input className="form-control" ref={addInput} />
+              <button className="btn btn-danger" onClick={handleAdd}>
+                Add
+              </button>
             </div>
           </div>
-        )
-        }
+        )}
       </div>
     </>
   );
